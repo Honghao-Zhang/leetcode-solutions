ae7afeab7c7372ac38b18f34b5eee907f83de295
Fridrich <fridrichzhangtubs@outlook.com>
2022-11-06 21:13:28 +0100

    solved rotting oranges

--- include/breadth_first_search/bfs.h
+++ include/breadth_first_search/bfs.h
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-namespace dp
+namespace bfs
 {
     class Solution
     {
--- test/p994_rotting_oranges_test.cpp
+++ test/p994_rotting_oranges_test.cpp
@@ -1,11 +1,36 @@
-#include "dynamic_programming/dp.h"
+#include "breadth_first_search/bfs.h"
 #include <gtest/gtest.h>
 
-TEST(RottingOranges, no_rotting)
+TEST(RottingOranges, example_1)
+{
+    std::vector<std::vector<int>> grids{{2, 1, 1},
+                                        {1, 1, 0},
+                                        {0, 1, 1}};
+    bfs::Solution obj;
+    EXPECT_EQ(4, obj.orangesRotting(grids));
+}
+
+TEST(RottingOranges, example_2)
+{
+    std::vector<std::vector<int>> grids{{2, 1, 1},
+                                        {0, 1, 1},
+                                        {1, 0, 1}};
+    bfs::Solution obj;
+    EXPECT_EQ(-1, obj.orangesRotting(grids));
+}
+
+TEST(RottingOranges, example_3)
+{
+    std::vector<std::vector<int>> grids{{0, 2}};
+    bfs::Solution obj;
+    EXPECT_EQ(0, obj.orangesRotting(grids));
+}
+
+TEST(RottingOranges, no_rotten)
 {
     std::vector<std::vector<int>> grids{{1, 1, 1},
-                                        {1, 1, 1},
-                                        {1, 1, 1}};
-    dp::Solution obj;
-    EXPECT_EQ(0, obj.orangesRotting(grids));
+                                        {0, 1, 1},
+                                        {1, 0, 1}};
+    bfs::Solution obj;
+    EXPECT_EQ(-1, obj.orangesRotting(grids));
 }
