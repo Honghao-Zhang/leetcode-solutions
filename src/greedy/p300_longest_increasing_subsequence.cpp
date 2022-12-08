8811ca11b32f0b25af939f210ea9cefb4bcf9c36
Fridrich <fridrichzhangtubs@outlook.com>
2022-12-08 20:25:53 +0100

    return the valid longest subsequence

--- src/greedy/p300_longest_increasing_subsequence.cpp
+++ src/greedy/p300_longest_increasing_subsequence.cpp
@@ -27,6 +27,79 @@ int solution_1(vector<int> &nums)
     return dp.size();
 }
 
+int solution_1_with_path(vector<int> &nums, vector<int> &LIS)
+{
+    // LIS is the longest increasing subsequence
+    int n = nums.size();
+    vector<int> subsequences, indices;
+    vector<int> traces(n, -1);
+    for (auto idx = 0; idx < n; idx++)
+    {
+        if (subsequences.empty() || subsequences.back() < nums[idx])
+        {
+            if (!subsequences.empty())
+                traces[idx] = indices.back();
+            subsequences.push_back(nums[idx]);
+            indices.push_back(idx);
+        }
+        else
+        {
+            auto tgt = lower_bound(subsequences.begin(), subsequences.end(), nums[idx]) - subsequences.begin();
+            if (tgt > 0)
+            {
+                traces[idx] = indices[tgt - 1];
+            }
+            subsequences[tgt] = nums[idx];
+            indices[tgt] = idx;
+        }
+    }
+    int track = indices.back();
+    while (track != -1)
+    {
+        LIS.push_back(nums[track]);
+        track = traces[track];
+    }
+    reverse(LIS.begin(), LIS.end());
+    return subsequences.size();
+}
+
+int solution_1_with_path_updated(vector<int> &nums, vector<int> &LIS)
+{
+    int n = nums.size();
+    vector<pair<int, int>> subsequences; // subsequences of (index, val) pair
+    vector<int> traces(n, -1);
+    subsequences.push_back({0, nums[0]});
+    for (auto i = 1; i < n; i++)
+    {
+        if (nums[i] > subsequences.back().second)
+        {
+            traces[i] = subsequences.back().first;
+            subsequences.push_back({i, nums[i]});
+        }
+        else
+        {
+            int j = lower_bound(subsequences.begin(), subsequences.end(), pair<int, int>(i, nums[i]),
+                                [](const pair<int, int> &p1, const pair<int, int> &p2)
+                                { return p1.second < p2.second; }) -
+                    subsequences.begin();
+
+            if (j > 0)
+            {
+                traces[i] = subsequences[j - 1].first; // the index of the current previous element in the subsequence
+            }
+            subsequences[j] = {i, nums[i]};
+        }
+    }
+    int idx = subsequences.back().first;
+    while (idx != -1)
+    {
+        LIS.push_back(nums[idx]);
+        idx = traces[idx];
+    }
+    reverse(LIS.begin(), LIS.end());
+    return subsequences.size();
+}
+
 // solution 2: brute force,
 // not work for example [0, 1, 0, 3, 2, 3], the first 3 will always block first 2 from being pushed into vector
 int solution_2_not_work(vector<int> &nums)
@@ -78,7 +151,7 @@ int solution_2(vector<int> &nums)
 
 // solution 3: monotonic queue,
 // not work for example [0, 1, 0, 3, 2, 3], the second 0 will always flush interval [0, 1] in the queue
-int solution_3(vector<int> &nums)
+int solution_3_not_work(vector<int> &nums)
 {
     vector<int> indices;
     size_t length = 0;
@@ -96,5 +169,6 @@ int solution_3(vector<int> &nums)
 
 int Solution::lengthOfLIS(std::vector<int> &nums)
 {
-    return solution_2(nums);
+    vector<int> path;
+    return solution_1_with_path_updated(nums, path);
 }
